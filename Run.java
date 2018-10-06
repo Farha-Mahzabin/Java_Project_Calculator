178b6eb17888a91801ed07cee4f3b3c28bf5ec6e
Farha-Mahzbin <zabin.farha.01@gmail.com>
2018-10-06 17:10:42 +0600

    initialization

---
+++ Run.java
@@ -0,0 +1,20 @@
+import javax.swing.SwingUtilities;
+
+import Controller.Controller;
+import Model.Model;
+import View.View;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            public void run() {
+            	
+            }
+        });
+
+    }
+
+}
